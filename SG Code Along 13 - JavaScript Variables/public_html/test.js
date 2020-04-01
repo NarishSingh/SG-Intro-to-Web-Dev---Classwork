f20c81ea618ddecbb71cb37cd76f2b80aa272db0
Narish Singh <narish.singh786@gmail.com>
2020-03-31 22:15:11 -0400

    Added SG Code Along #13 - JS Variables

---
+++ SG Code Along 13 - JavaScript Variables/public_html/test.js
@@ -0,0 +1,15 @@
+/*
+ * Run from IDE
+ * open "inspect" from right click in chrome
+ * go to console and you'll see stuff printed
+ */
+
+var a=12*3;
+console.log("a = " + a);
+
+var foo=20/5;
+console.log("foo = " + foo);
+
+foo=false;
+console.log("foo = " + foo);
+
