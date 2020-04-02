4c627e8ef7ca5a4177a69fc6927ce4f613d0e4f5
Narish Singh <narish.singh786@gmail.com>
2020-04-01 21:41:31 -0400

    Added SG Code Along #18 - Functions

---
+++ SG Code Along 18 - Counting Characters/public_html/countingCharacters.js
@@ -0,0 +1,75 @@
+/* 
+ * char counter functions
+ */
+
+/** countingChars(stringToCount)
+ * Count chars in a string
+ * @param stringToCount
+ * @returns prints string and the number of chars in string
+ */
+function countingChars(stringToCount)
+{
+    console.log(stringToCount + " has " + stringToCount.length + " chars");
+}
+
+countingChars("cake");
+
+var portalString="The cake is a lie";
+countingChars(portalString);
+
+/** countingChars2(stringToCount, charToFind)
+ * counts for a char in string
+ * @param {string} stringToCount
+ * @param {string} charToFind - only 1 char
+ * @returns prints statements on args and reports num of char in string
+ */
+function countingChars2(stringToCount, charToFind)
+{
+    var charCt=0;
+    
+    for (var i = 0; i < stringToCount.length; i++) 
+    {
+        if(stringToCount[i]===charToFind)
+        {
+            charCt++;
+        }
+    }
+    
+    console.log("String to search in: " + stringToCount);
+    console.log("Char to find: " + charToFind);
+    console.log("Number of times the char appears: " + charCt);
+}
+
+countingChars2("xxaxxxbxx", "x");
+
+/** countingChars3(str, search)
+ * Count number of times a substring comes up
+ * @param {string} str
+ * @param {string} search
+ * @returns prints statements on args and reports occurences of a substring
+ * @returns {int} ct 
+ */
+function countingChars3(str, search)
+{
+    var ct=0;
+    var numChars=search.length;
+    
+    var lastIndex=str.lenght-numChars; //need to stop loop based on multiple chars
+    
+    for (var i = 0; i <= lastIndex; i++) 
+    {
+        var current=str.substring(i, i+numChars);
+        if (current==search) 
+        {
+            ct++;
+        }
+    }
+    
+//    console.log("String to search: ", + str);
+//    console.log("Char to find: ", + search);
+//    console.log("Number of times the char appears: ", + ct);
+    return ct;
+}
+
+countingChars3("xxaxxxbxx", "xx");
+
