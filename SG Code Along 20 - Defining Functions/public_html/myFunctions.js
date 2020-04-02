253ba0c98bcfc11ea44351edac5dc1be403a5ff8
Narish Singh <narish.singh786@gmail.com>
2020-04-02 10:39:24 -0400

    Documentation for functions

--- SG Code Along 20 - Defining Functions/public_html/myFunctions.js
+++ SG Code Along 20 - Defining Functions/public_html/myFunctions.js
@@ -3,6 +3,12 @@
  */
 
 //1 - Addition
+/** addTwoNum(first, second)
+ * binary addition
+ * @param {Number|int|float} first - 1st number argument
+ * @param {Number|int|float} second - 2nd number argument
+ * @returns {Number|int|float} sum of arguments
+ */
 function addTwoNum(first, second)
 {
     return first+second;
@@ -19,6 +25,11 @@ result=addTwoNum(-53,40);
 console.log("-53+40 = %s", result);
 
 //2 - 1st char in string
+/** get1stChar(text)
+ * Process string and return 1st letter
+ * @param {string} text - string or char argument
+ * @returns {char|string} - first char in string, or alert on error
+ */
 function get1stChar(text)
 {
     if(text.length>=1)
@@ -38,6 +49,13 @@ console.log("'Starling': %s", get1stChar("Starling"));
 console.log("'Olympus Mons': %s", get1stChar("Olympus Mons"));
 
 //3 - Max of 3 arg's
+/** maxOf3(first, second, third)
+ * Find max in a 3 number series
+ * @param {Number|int|float} first - 1st number argument
+ * @param {Number|int|float} second - 1st number argument
+ * @param {Number|int|float} third - 1st number argument
+ * @returns {Number|int|float} - maximum in number argument series
+ */
 function maxOf3(first, second, third)
 {
     if(first>second && first>third)
