0c3c2e5d1662054981f9b5ad25d96f8b0dab466f
Narish Singh <narish.singh786@gmail.com>
2020-04-07 19:26:59 -0400

    Attempted using onclick, still doesn't work, need to revisit

--- SG Code Along 26 - Display Evens/public_html/evensInRange.js
+++ SG Code Along 26 - Display Evens/public_html/evensInRange.js
@@ -3,38 +3,48 @@
  */
 
 //get and cast user inputs to numbers
-var inputs=document.forms["inputs"];
-var start = Number(document.getElementById("start"));
-var end = Number(document.getElementById("end"));
-var step = Number(document.getElementById("step"));
-var results=document.getElementById("results");
-var displayEvens=document.getElementById("displayEvens");
+/*
+ var inputs=document.forms["inputs"];
+ var start = Number(document.getElementById("start"));
+ var end = Number(document.getElementById("end"));
+ var step = Number(document.getElementById("step"));
+ var results=document.getElementById("results");
+ var displayEvensBtn=document.getElementById("displayEvensBtn");
+ */
 
 /** validate()
  * Validate form
  * @returns {Boolean} always false to stop reload
  */
-function validate()
-{
-    inputs.className = "needs-validation";
+/*
+ function validate()
+ {
+ inputs.className = "needs-validation";
+ 
+ if (!inputs.checkValidity())
+ {
+ inputs.className = "was-validated";
+ return false;
+ }
+ 
+ //fill in spans for inputs and results
+ document.getElementById("userStart").innerText=start;
+ document.getElementById("userEnd").innerText=end;
+ document.getElementById("userStep").innerText=step;
+ document.getElementById("resultsArray").innerText=evensInStep();
+ 
+ results.style.display="block";
+ displayEvensBtn.innerText="New Range";
+ 
+ return false;
+ }
+ */
 
-    if (!inputs.checkValidity())
-    {
-        inputs.className = "was-validated";
-        return false;
-    }
-    
-    //fill in spans for inputs and results
-    document.getElementById("userStart").innerText=start;
-    document.getElementById("userEnd").innerText=end;
-    document.getElementById("userStep").innerText=step;
-    document.getElementById("resultsArray").innerText=evensInStep();
-    
-    results.style.display="block";
-    displayEvens.innerText="New Range";
 
-    return false;
-}
+//get and cast user inputs to numbers
+var start = Number(document.getElementById("num1").value);
+var end = Number(document.getElementById("num2").value);
+var step = Number(document.getElementById("step").value);
 
 /** evensInStep()
  * Push all possible step values in range in an array, then find even step values in range
@@ -59,4 +69,19 @@ function evensInStep()
     }
 
     return evenStepValues;
+}
+
+function calculate()
+{
+    //calculate
+    resultsArray = evensInStep();
+    console.log(resultsArray);
+    
+    for (var i = 0; i < resultsArray.length; i++)
+    {
+        document.getElementById("resultsArray").innerText=resultsArray[i];
+    }
+
+    //display results
+    document.getElementById("results").style.display = "block";
 }
