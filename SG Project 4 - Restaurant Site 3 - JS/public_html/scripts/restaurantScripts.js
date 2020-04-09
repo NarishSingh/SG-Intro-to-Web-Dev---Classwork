3aa5a877b766e071caeec867fa6c4e043ce3bc96
Narish Singh <narish.singh786@gmail.com>
2020-04-09 17:49:31 -0400

    fixed footer, changed pictures, added JS function for main page pic, finished carousel, made sticky navbar for menu, working on its scrollspy

--- SG Project 4 - Restaurant Site 3 - JS/public_html/scripts/restaurantScripts.js
+++ SG Project 4 - Restaurant Site 3 - JS/public_html/scripts/restaurantScripts.js
@@ -3,5 +3,21 @@ SG Self Assessment - Restaurant Site v3
 JavaScript Sheet
 */
 
+/*FRONT PAGE IMAGE FUNCTIONS*/
+/** fullOpacity()
+ * on hover, bring image to full opacity
+ * @returns {void}
+ */
+function fullOpacity()
+{
+    document.getElementById("frontPageImage").style.opacity="100%";
+}
 
-
+/** restoreOpacity()
+ * on mouse out, return to original opacity
+ * @returns {void}
+ */
+function restoreOpacity()
+{
+    document.getElementById("frontPageImage").style.opacity="50%";
+}
